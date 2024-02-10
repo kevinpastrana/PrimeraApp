8cf27b4aeb4f2fbaf09c8f93afa88bee4684aa0e
Kevin Pastrana <u20232215370@usco.edu.co>
2024-02-09 21:12:46 -0500

    Mi primer commit

---
+++ src/PrimeraApp/HolaMundo.java
@@ -0,0 +1,10 @@
+package PrimeraApp;
+/**
+ *
+ * @author Kevin Felipe
+ */
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+}
